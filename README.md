5ca9853a5d3c0252b77ccb5cefbd6a87c2e3c72e
Daffa Audya Pramana <daffaaudyapramana03@gmail.com>
2024-01-08 17:32:27 +0700

    update banyak banyak

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # booking_lapangan
 
-A new Flutter project.
+A new Flutter project
---
+++ lib/Data/data_booking.dart
@@ -0,0 +1,46 @@
+import 'package:booking_lapangan/model/booking_model.dart';
+
+List<Konser> concertsData = [
+  Booking(
+    namalapangan: '1',
+    tanggal: 'December 31, 2023',
+    jammulai: '8:00 PM',
+    totaljammain: '2 jam',
+    harga: '300.000',
+  ),
+  Booking(
+    namalapangan: '2',
+    tanggal: 'December 31, 2023',
+    jammulai: '8:00 PM',
+    totaljammain: '2 jam',
+    harga: '300.000',
+  ),
+  Booking(
+    namalapangan: '2',
+    tanggal: 'December 31, 2023',
+    jammulai: '8:00 PM',
+    totaljammain: '2 jam',
+    harga: '300.000',
+  ),
+  Booking(
+    namalapangan: '3',
+    tanggal: 'December 31, 2023',
+    jammulai: '8:00 PM',
+    totaljammain: '2 jam',
+    harga: '300.000',
+  ),
+  Booking(
+    namalapangan: '1',
+    tanggal: 'December 31, 2023',
+    jammulai: '8:00 PM',
+    totaljammain: '2 jam',
+    harga: '300.000',
+  ),
+  Booking(
+    namalapangan: '2',
+    tanggal: 'December 31, 2023',
+    jammulai: '8:00 PM',
+    totaljammain: '2 jam',
+    harga: '300.000',
+  ),
+];
