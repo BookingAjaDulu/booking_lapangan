0946e4ed37d1c5a56d325991098b16d18a15cb0a
Daffa Audya Pramana <daffaaudyapramana03@gmail.com>
2024-01-22 01:10:44 +0700

    update title pada main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -39,7 +39,7 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'KasKu',
+      title: 'Lapangan',
       debugShowCheckedModeBanner: false,
       home: page,
     );
